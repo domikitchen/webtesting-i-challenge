3bb38415d5c3e58c499f7cbdfdc854999042c970
Dominique K Kitchenakow <domikitchen@users.noreply.github.com>
2020-08-19 18:14:14 -0500

    completed mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,35 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
-}
+  if(item.enhancement === 20){
+    return item;
+  }
+  else {
+    item.enhancement = item.enhancement + 1;
+    return item;
+  }
+};
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement < 15) {
+    item.durability = item.durability - 5;
+    return item;
+  }
+  else if(item.enhancement >= 15) {
+    item.durability = item.durability - 10;
+    if(item.enhancement > 16) {
+      item.enhancement = item.enhancement - 1;
+      return item;
+    }
+    else {
+      return item;
+    }
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  item.durability = 100;
+  return item;
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,103 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+
+it('should reset the durability to 100', () => {
+    const item = {
+        "name": 'thing',
+        "durability": 30,
+        "enhancement": 3
+    };
+    
+    const newItem = {
+        "name": 'thing',
+        "durability": 100,
+        "enhancement": 3
+    };
+
+    expect(enhancer.repair(item)).toStrictEqual(newItem);
+});
+
+it('should increase the enhancement by 1', () => {
+    const item = {
+        "name": 'thing',
+        "durability": 30,
+        "enhancement": 2
+    }
+
+    const newItem = {
+        "name": 'thing',
+        "durability": 30,
+        "enhancement": 3
+    };
+    
+    const largerItem = {
+        "name": 'thing',
+        "durability": 30,
+        "enhancement": 20
+    }
+
+    const largerNewItem = {
+        "name": 'thing',
+        "durability": 30,
+        "enhancement": 20
+    };
+
+    expect(enhancer.success(item)).toStrictEqual(newItem);
+    expect(enhancer.success(largerItem)).toStrictEqual(largerNewItem);
+}); 
+
+it('enhancement fail function should work', () => {
+    const item = {
+        "name": 'thing',
+        "durability": 30,
+        "enhancement": 3
+    };
+
+    const newItem = {
+        "name": 'thing',
+        "durability": 25,
+        "enhancement": 3
+    };
+
+    const itemTwo = {
+        "name": 'thing',
+        "durability": 30,
+        "enhancement": 15
+    };
+    
+    const newItemTwo = {
+        "name": 'thing',
+        "durability": 20,
+        "enhancement": 15
+    };
+
+    const itemThree = {
+        "name": 'thing',
+        "durability": 20,
+        "enhancement": 16
+    };
+    
+    const newItemThree = {
+        "name": 'thing',
+        "durability": 10,
+        "enhancement": 16
+    };
+
+    const itemFour = {
+        "name": 'thing',
+        "durability": 30,
+        "enhancement": 18
+    };
+    
+    const newItemFour = {
+        "name": 'thing',
+        "durability": 20,
+        "enhancement": 17
+    };
+
+    expect(enhancer.fail(item)).toStrictEqual(newItem);
+    expect(enhancer.fail(itemTwo)).toStrictEqual(newItemTwo);
+    expect(enhancer.fail(itemThree)).toStrictEqual(newItemThree);
+    expect(enhancer.fail(itemFour)).toStrictEqual(newItemFour);
+})
